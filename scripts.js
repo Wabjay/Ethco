97830c45b4b52aa91cf54bda601cd19ad793ff5b
Abdulwahab Jimoh <omoniyi.success@gmail.com>
2022-07-25 18:15:37 -0500

    Init commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+var acc = document.getElementsByClassName("faq_title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = (panel.scrollHeight) + "px";
+      console.log(panel.style.maxHeight)
+    } 
+  });
+}
+
+
+// function switcher() {
+   const shop = document.getElementById("shopper");
+   const store = document.getElementById("storeowners");
+   var pageSwitch = document.querySelectorAll(".label");
+
+   for (let i = 0; i < pageSwitch.length; i++) {
+       pageSwitch[i].addEventListener("click", function(){
+           if(pageSwitch[i].textContent === "For shoppers"){
+                // console.log(pageSwitch[i].innerHTML)
+        console.log("first")
+        store.classList.add("display");
+        shop.classList.remove("display")
+    } else if(pageSwitch[i].textContent === "For storeowners"){
+        console.log("second")
+        store.classList.remove("display");
+        shop.classList.add("display")
+    }
+        })
+    }
+// }
+// switcher()
