29b127282cb37448c6a0032a5f162ba0093d8557
Abdulwahab Jimoh <omoniyi.success@gmail.com>
2022-07-28 12:47:51 -0500

    Script

--- scripts.js
+++ scripts.js
@@ -1,62 +1,59 @@
-var grid = document.querySelector(".grid")
-var display_toggle = document.querySelector(".display_toggle")
-grid.addEventListener("click", function(){
-  console.log(display_toggle.classList)
-  display_toggle.classList.toggle("toggle")
-})
+// NAVBAR DROPDOWN
+var grid = document.querySelector(".grid");
+var display_toggle = document.querySelector(".display_toggle");
+grid.addEventListener("click", function () {
+  console.log(display_toggle.classList);
+  display_toggle.classList.toggle("toggle");
+});
 
+// FAQ
 var acc = document.getElementsByClassName("faq_title");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = (panel.scrollHeight) + "px";
-      console.log(panel.style.maxHeight)
-    } 
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      console.log(panel.style.maxHeight);
+    }
   });
 }
 
+//  SWITCHER
+const shop = document.getElementById("shopper");
+const store = document.getElementById("storeowners");
+var pageSwitch = document.querySelectorAll(".label");
 
-
-
-// function switcher() {
-   const shop = document.getElementById("shopper");
-   const store = document.getElementById("storeowners");
-   var pageSwitch = document.querySelectorAll(".label");
-
-   for (let i = 0; i < pageSwitch.length; i++) {
-       pageSwitch[i].addEventListener("click", function(){
-           if(pageSwitch[i].textContent === "For shoppers"){
-                // console.log(pageSwitch[i].innerHTML)
-        console.log("first")
-        store.classList.add("display");
-        shop.classList.remove("display")
-    } else if(pageSwitch[i].textContent === "For storeowners"){
-        console.log("second")
-        store.classList.remove("display");
-        shop.classList.add("display")
+for (let i = 0; i < pageSwitch.length; i++) {
+  pageSwitch[i].addEventListener("click", function () {
+    if (pageSwitch[i].textContent === "For shoppers") {
+      // console.log(pageSwitch[i].innerHTML)
+      console.log("first");
+      store.classList.add("display");
+      shop.classList.remove("display");
+    } else if (pageSwitch[i].textContent === "For storeowners") {
+      console.log("second");
+      store.classList.remove("display");
+      shop.classList.add("display");
     }
-        })
-    }
-// }
-// switcher()
+  });
+}
 
-
-const pop = document.querySelectorAll('.pop');
+// Modal Box
+const pop = document.querySelectorAll(".pop");
 const popup = document.getElementById("popup");
-const close_popup = document.querySelector('.close_popup');
 for (let i = 0; i < pop.length; i++) {
-  pop[i].addEventListener("click", function(){
-    popup.style.display = "block"
-  })
-};
+  pop[i].addEventListener("click", function () {
+    popup.style.display = "block";
+  });
+}
 
-  close_popup.addEventListener("click", function(){
-    popup.style.display = 'none';
-     }
-  );
+// Close Modal Bos
+const close_popup = document.querySelector(".close_popup");
+close_popup.addEventListener("click", function () {
+  popup.style.display = "none";
+});
